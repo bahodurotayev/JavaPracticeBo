f5aa4af114b79311fd289d3cba98b5ab212bc194
bahodurotayev <tjkbaha@gmail.com>
2022-09-14 18:59:28 -0400

    day 12

--- src/Day11/Java/PracticeOCA1.java
+++ src/Day11/Java/PracticeOCA1.java
@@ -6,8 +6,10 @@ public class PracticeOCA1 {
         int y = 1;
         if (x++ < ++y) {
             System.out.print("Hello ");
+
         }else {
             System.out.print("Welcome ");
+
         }
         System.out.println("Log " + x + ":" + y);
 
--- src/Day12/Practice/Mentoring_08_24_2022/Task3.java
+++ src/Day12/Practice/Mentoring_08_24_2022/Task3.java
@@ -5,13 +5,7 @@ import java.util.Scanner;
 public class Task3 {
     public static void main(String[] args) {
 
-        /*Task3
-        * * Write a Java program that takes a number and set thousands separator in that number. 1000 => 1,000
-        * example: 100 => 100
-        *                1000 => 1,000 / len = 4  len-3
-        *                10000 => 10,000
-        *                100000 => 100,000
-         * */
+
 
         Scanner scan = new Scanner(System.in);
         String result ="";
