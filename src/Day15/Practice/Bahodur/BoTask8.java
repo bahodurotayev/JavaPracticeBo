d07a4f6e7fdcd38231f445856fa29bfcd9b189a7
bahodurotayev <tjkbaha@gmail.com>
2022-09-17 12:14:01 -0400

    day15 groupProject

--- src/Day15/Practice/Bahodur/BoTask8.java
+++ src/Day15/Practice/Bahodur/BoTask8.java
@@ -1,5 +1,9 @@
 package Day15.Practice.Bahodur;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class BoTask8 {
     public static void main(String[] args) {
                         /*Task8.
@@ -11,6 +15,27 @@ Example:
 12344321 => true
 1236541 => false
 */
-        
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter number - ");
+        int number = scan.nextInt();
+        String convertedNumber = Integer.toString(number);
+        System.out.println("Reverse number is -" +reverse(convertedNumber));
+
+        if(convertedNumber.equals(reverse(convertedNumber))){
+            System.out.println("true");
+        }else {
+            System.out.println("false");
+        }
+    }
+    static String reverse (String str){
+        int count = str.length();
+        String newStr = "";
+        while (count > 0){
+            newStr +=str.charAt(count -1);
+            count--;
+        }
+
+        return newStr;
     }
 }
