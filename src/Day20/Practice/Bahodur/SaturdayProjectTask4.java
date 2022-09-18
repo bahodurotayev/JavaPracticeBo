3fd20301f7b91e5b243ceabc5ce55abb126dee01
bahodurotayev <tjkbaha@gmail.com>
2022-09-18 18:43:15 -0400

    day19

--- src/Day20/Practice/Bahodur/SaturdayProjectTask4.java
+++ src/Day20/Practice/Bahodur/SaturdayProjectTask4.java
@@ -1,5 +1,8 @@
 package Day20.Practice.Bahodur;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class SaturdayProjectTask4 {
     public static void main(String[] args) {
              /*
@@ -10,6 +13,36 @@ public class SaturdayProjectTask4 {
             3. Print the difference between the largest and smallest values in the array. (largest - smallest = difference)
             [10, 3, 5, 6, 8, 7, 5, 6, 8, 7] → 7 (observe 10-3 = 7)
    */
+        int [] arr = new int[10];
+        Random rnd = new Random();
+        for (int i = 0; i <arr.length ; i++) {
+            arr[i] = (int)(Math.random() * 20);
+        }
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
 
+        int max = arr[arr.length-1];
+        int min = arr[0];
+
+        System.out.println("min = " + min);
+        System.out.println("max = " + max);
+        System.out.println("Difference = " + (max - min));
+
+
+
+
+//        int min = arr[0];
+//        int max = arr[0];
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            if (max > arr[i]) {
+//                max =arr[i];
+//            }else if(min < arr[i]){
+//                min = arr[i];
+//            }
+//        }
+//        System.out.println(Arrays.toString(arr));
+//        System.out.println(min);
+//        System.out.println(max);
     }
 }
--- src/Day20/Practice/Bahodur/SaturdayProjectTask5.java
+++ src/Day20/Practice/Bahodur/SaturdayProjectTask5.java
@@ -2,6 +2,14 @@ package Day20.Practice.Bahodur;
 
 public class SaturdayProjectTask5 {
     public static void main(String[] args) {
+/*
+        Task5:
+        Create a char array
+        The array has a random length between 2 and 22 (inclusive)
+                The array should only have random capital letters.
+                Print the array.
+
+ */
 
     }
 }
