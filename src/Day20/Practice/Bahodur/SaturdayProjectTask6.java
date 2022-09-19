90caa96e3bf956df67bbed7848be27005f4f5632
bahodurotayev <tjkbaha@gmail.com>
2022-09-18 20:19:26 -0400

    day19

--- src/Day20/Practice/Bahodur/SaturdayProjectTask6.java
+++ src/Day20/Practice/Bahodur/SaturdayProjectTask6.java
@@ -1,4 +1,25 @@
 package Day20.Practice.Bahodur;
 
+import java.util.Arrays;
+
 public class SaturdayProjectTask6 {
+    /*
+        Task6:
+        Given a String array as ["bird", "dog", "cat", "door"]
+        Create a new array named as arrRev.
+                Reverse the order of the elements of the String array and store in the arrRev.
+        Print the arrRev
+        Sample:
+        Given ["bird", "dog", "cat", "door"] -> prints ["door", "cat", "dog" , "bird"]
+ */
+    public static void main(String[] args) {
+        String[] str = {"bird", "dog", "cat", "door"};
+        String[] reverse = new String[str.length];
+        System.out.println(Arrays.toString(str));
+
+        for (int i = 0, j = str.length-1; i <reverse.length ; i++, j--) {
+            reverse[i] = str[j];
+        }
+        System.out.println(Arrays.toString(reverse));
+    }
 }
