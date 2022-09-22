4f89e84a4cb446c6bd4f20451ae30f267d1e089d
bahodurotayev <tjkbaha@gmail.com>
2022-09-22 18:10:46 -0400

    day29&day30

--- src/Day30/Practice/Bahodur/Task2.java
+++ src/Day30/Practice/Bahodur/Task2.java
@@ -1,5 +1,9 @@
 package Day30.Practice.Bahodur;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Task2 {
       /*
 
@@ -8,6 +12,26 @@ public class Task2 {
     return the list of list of strings
      */
       public static void main(String[] args) {
+          ArrayList<ArrayList<String>> listOfList = new ArrayList<>();
+          listOfList.add(new ArrayList<>(Arrays.asList("bahodur", "alisher", "arda")));
+          listOfList.add(new ArrayList<>(Arrays.asList("furkat", "dilnoza", "andrea")));
 
+          Task2 t2 = new Task2();
+          ArrayList<ArrayList<String>> listOfList2 = t2.listOfString(listOfList);
+          System.out.println(listOfList2);
+
+      }
+      ArrayList<ArrayList<String>> listOfString(ArrayList<ArrayList<String>> listOfList){
+
+          for (int i = 0; i < listOfList.size(); i++) {
+
+              for (int j = 0; j < listOfList.get(i).size(); j++) {
+                  String strToUpperCase = listOfList.get(i).get(j).toUpperCase();
+                  listOfList.get(i).set(j, strToUpperCase);
+              }
+
+          }
+
+          return listOfList;
       }
 }
---
+++ src/Day30/Practice/Bahodur/Task3.java
@@ -0,0 +1,7 @@
+package Day30.Practice.Bahodur;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+    }
+}
