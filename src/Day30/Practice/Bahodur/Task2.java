fff96e7adf99f9e0dc314c2b79c5805b87c83ffe
bahodurotayev <tjkbaha@gmail.com>
2022-09-22 17:48:50 -0400

    day29&day30

--- src/Day30/Practice/Bahodur/Task2.java
+++ src/Day30/Practice/Bahodur/Task2.java
@@ -6,8 +6,8 @@ public class Task2 {
     Create a method, which will have list of list of Strings as a parameter.
     Convert all String character to upper case.
     return the list of list of strings
-
-    9:30 - 9:40
-
      */
+      public static void main(String[] args) {
+
+      }
 }
