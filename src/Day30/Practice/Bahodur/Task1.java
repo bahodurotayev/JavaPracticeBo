36a259b330b0cd232ccaa729ed2cab345f34c300
bahodurotayev <tjkbaha@gmail.com>
2022-09-22 17:37:38 -0400

    day29&day30

--- src/Day30/Practice/Bahodur/Task1.java
+++ src/Day30/Practice/Bahodur/Task1.java
@@ -1,4 +1,39 @@
 package Day30.Practice.Bahodur;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Task1 {
+     /*
+    Create listOfLists of String.
+    1. have at least 3 lists within listOfLists
+    2. Size of the list or values could be anything.
+    3. print the list of lists, using enhanced for loop or regular for loop.
+    10 min - 9.0
+     */
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> listOfList = new ArrayList<>();
+        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(4, 5, 6, 7));
+        ArrayList<Integer> list3 = new ArrayList<>(Arrays.asList(8, 9, 0, 1));
+
+        listOfList.add(list1);
+        listOfList.add(list2);
+        listOfList.add(list3);
+
+        for (ArrayList<Integer> eachList: listOfList) {
+            for (Integer eachNum:eachList) {
+                System.out.print(eachNum + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("2 way ============================================");
+        for (ArrayList<Integer> eachList: listOfList) {
+            eachList.forEach(eachNum -> System.out.print(eachNum + " "));
+            System.out.println();
+        }
+
+    }
 }
---
+++ src/Day30/Practice/Bahodur/Task2.java
@@ -0,0 +1,13 @@
+package Day30.Practice.Bahodur;
+
+public class Task2 {
+      /*
+
+    Create a method, which will have list of list of Strings as a parameter.
+    Convert all String character to upper case.
+    return the list of list of strings
+
+    9:30 - 9:40
+
+     */
+}
