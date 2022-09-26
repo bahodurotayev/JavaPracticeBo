0b6ec3ccd61209457f1c5cf897d67baf5882cd06
bahodurotayev <tjkbaha@gmail.com>
2022-09-25 22:23:09 -0400

    day34 saturday Project

--- src/Day34/Practice/Bahodur/Task3.java
+++ src/Day34/Practice/Bahodur/Task3.java
@@ -1,4 +1,40 @@
 package Day34.Practice.Bahodur;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Task3 {
+    /*
+    *  Create a method, which will return a ArrayList<Integer> and parameter is a ArrayList<Integer>.
+        The method should sort the ArrayList.
+
+        Note: Don`t use any of built-in sort methods.
+
+        Example:
+        {5,4,2,1,0,3}
+
+        Result: {0,1,2,3,4,5}*/
+    static ArrayList<Integer> sortArray (ArrayList<Integer> list){
+
+        for (int i = 0; i <list.size() ; i++) {
+            for (int j = list.size() - 1; j > i ; j--) {
+                    // 5 4 2 1 0 3
+                    // 0 1 2 3 4 5
+
+                int tem = list.get(i);
+                if(list.get(i) > list.get(j)){
+                    list.set(i, list.get(j));
+                    list.set(j, tem);
+                }
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(5,4,2,1,0,3));
+        ArrayList<Integer> list1 = sortArray(list);
+        System.out.println(list1);
+    }
 }
---
+++ src/Day34/Practice/Bahodur/Task4.java
@@ -0,0 +1,22 @@
+package Day34.Practice.Bahodur;
+
+public class Task4 {
+    public static void main(String[] args) {
+        /* Task 4
+        1. Create a method which will return ArrayList<Integer> with no parameters.
+           Add 10 random numbers between 0 and 10 to the list.
+
+        2. Create a method which will return true or false.
+           Parameter is ArrayList<Integer>, and int (X).
+           Determine whether there exist two elements in the list whose sum is exactly X.
+
+          Example:
+
+          {1, 4, 45, 6, 10, 8} and X = 16
+          Result = true;
+
+          {1, 2, 4, 3, 6} and X = 11
+          Result = false;*/
+
+    }
+}
