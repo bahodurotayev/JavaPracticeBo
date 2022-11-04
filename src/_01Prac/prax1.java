fed1fdc37378807f128a438763ed09fa7ee7de10
bahodurotayev <tjkbaha@gmail.com>
2022-11-04 17:05:03 -0400

    day

--- src/_01Prac/prax1.java
+++ src/_01Prac/prax1.java
@@ -2,8 +2,24 @@ package _01Prac;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.Stack;
+
 public class prax1 {
     public static void main(String[] args) {
-        System.out.println("");
+        String[] strs = {"flower", "flow", "flight"};
+        String s = longestCommonPrefix(strs);
+        System.out.println("longestCommonPrefix = " + s);
+    }
+    static String longestCommonPrefix(String[] strs){
+        if(strs.length == 0){
+            return "";
+        }
+        String prefix =strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+        return prefix;
     }
 }
