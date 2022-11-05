e397c5ec7d1bc938d769be415a10fc0e6b1e737d
bahodurotayev <tjkbaha@gmail.com>
2022-11-05 04:31:50 -0400

    day

--- src/_01Prac/prax1.java
+++ src/_01Prac/prax1.java
@@ -2,13 +2,16 @@ package _01Prac;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.Arrays;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 public class prax1 {
     public static void main(String[] args) {
-        String[] strs = {"flower", "flow", "flight"};
-        String s = longestCommonPrefix(strs);
+        String[] strs = {"flower", "flow", "fliwht"};
+        String s = longestCommonPrefix2(strs);
         System.out.println("longestCommonPrefix = " + s);
+
     }
     static String longestCommonPrefix(String[] strs){
         if(strs.length == 0){
@@ -22,4 +25,24 @@ public class prax1 {
         }
         return prefix;
     }
+    public static String longestCommonPrefix2(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
+
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        int c = 0;
+        while(c < first.length())
+        {
+            if (first.charAt(c) == last.charAt(c)){
+                c++;
+            }
+            else {
+                break;
+            }
+        }
+        return c == 0 ? "" : first.substring(0, c);
+    }
 }
+
